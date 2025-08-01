5cc32d30e61cb1b2a15ea4de65fced971caafd81
unknown <plamedimandiangu937@gmail.com>
2025-08-01 11:43:27 +0200

    Add subqueries for average rating and user bookings

--- subqueries.sql
+++ subqueries.sql
@@ -1,18 +1,13 @@
--- 1. Subquery: Find all properties where the average rating is greater than 4.0
-SELECT *
-FROM properties
+-- Query 1: Find all properties where average rating is greater than 4.0 using a subquery
+SELECT * FROM properties
 WHERE id IN (
-    SELECT property_id
-    FROM reviews
+    SELECT property_id FROM reviews
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
 );
 
--- 2. Correlated Subquery: Find users who have made more than 3 bookings
-SELECT *
-FROM users u
+-- Query 2: Find users who have made more than 3 bookings using a correlated subquery
+SELECT DISTINCT u.id, u.name FROM users u
 WHERE (
-    SELECT COUNT(*)
-    FROM bookings b
-    WHERE b.user_id = u.id
+    SELECT COUNT(*) FROM bookings b WHERE b.user_id = u.id
 ) > 3;
