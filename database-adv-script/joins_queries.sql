d28a4024c0c26210bb7998db33c960edf12f2472
Plamedi Mandiangu <62853411+PlamediMandiangu@users.noreply.github.com>
2025-08-01 08:59:35 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,7 +8,8 @@ SELECT bookings.id AS booking_id,
        bookings.check_in,
        bookings.check_out
 FROM bookings
-INNER JOIN users ON bookings.user_id = users.id;
+INNER JOIN users ON bookings.user_id = users.id
+ORDER BY bookings.id;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT properties.id AS property_id,
@@ -17,7 +18,8 @@ SELECT properties.id AS property_id,
        reviews.rating,
        reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings
 -- (Note: Some SQL engines like MySQL don't support FULL OUTER JOIN natively)
@@ -26,7 +28,8 @@ SELECT users.id AS user_id,
        bookings.id AS booking_id,
        bookings.property_id
 FROM users
-FULL OUTER JOIN bookings ON users.id = bookings.user_id;
+FULL OUTER JOIN bookings ON users.id = bookings.user_id
+ORDER BY users.id, bookings.id;
 
 -- If FULL OUTER JOIN is not supported, use UNION of LEFT and RIGHT JOINs:
 -- SELECT users.id AS user_id,
@@ -41,4 +44,5 @@ FULL OUTER JOIN bookings ON users.id = bookings.user_id;
 --        bookings.id AS booking_id,
 --        bookings.property_id
 -- FROM users
--- RIGHT JOIN bookings ON users.id = bookings.user_id;
+-- RIGHT JOIN bookings ON users.id = bookings.user_id
+-- ORDER BY user_id, booking_id;
